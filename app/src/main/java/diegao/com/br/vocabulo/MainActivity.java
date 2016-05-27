c0d09a468aafd49d47f483e871d56facd3af02b3
Marcelo Araujo <marceloaraujo14@hotmail.com>
2016-05-27 02:05:00 -0300

    Added fragments resource

--- app/src/main/java/diegao/com/br/vocabulo/MainActivity.java
+++ app/src/main/java/diegao/com/br/vocabulo/MainActivity.java
@@ -1,27 +1,36 @@
 package diegao.com.br.vocabulo;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class MainActivity extends Activity implements GestureDetector.OnGestureListener{
+
 
     private GestureDetector detector;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         this.detector = new GestureDetector(this);
 
@@ -29,6 +38,14 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         vocabulo.setWord("Anacrônico");
         vocabulo.setDefinition("Divergencia de tempo / Época diferente / Discrepância temporal");
         vocabulo.setUseExemple("Um smartphone no ano de 1920 é algo completamente anacrônico");
+
+
+        FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().add(R.id.container,new CardFrontFragment()).commit();
+
+
+
+
     }
 
     @Override
@@ -79,12 +96,18 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
         if(e1.getX()-e2.getX()>100 && Math.abs(velocityX)>200){
 
-            Log.i("INFORMATION ", "###### vai flipar #####");
+
+            FragmentManager fm = getFragmentManager();
+            fm.beginTransaction().add(R.id.container,new CardBackFragment()).commit();
+
+            Log.i("INFORMATION ", "###### FLIPOU!!! #####");
             //Intent intent = new Intent(this,Screen3.class);
             //intent.putExtra("statuspoint",statuspoint);
             //startActivity(intent);
         }
         return false;
     }
+
+
 
 }
