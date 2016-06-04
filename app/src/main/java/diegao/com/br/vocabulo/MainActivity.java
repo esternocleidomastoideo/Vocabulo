688b844b372a280d15f7f90f113f21c50ed9160c
Marcelo Araujo <marceloaraujo14@hotmail.com>
2016-06-04 03:19:05 -0300

    Fix error animation flip

--- app/src/main/java/diegao/com/br/vocabulo/MainActivity.java
+++ app/src/main/java/diegao/com/br/vocabulo/MainActivity.java
@@ -17,9 +17,6 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
